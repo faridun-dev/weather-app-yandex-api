72eb9ed9cbe093c3ec87579550a71b9b0ea6fb9d
faridun-dev <faridunf200630@gmail.com>
2024-09-12 17:59:40 +0500

    Added weather model

---
+++ lib/models/weather.dart
@@ -0,0 +1,33 @@
+class Weather {
+  final String cityName;
+  final String condition;
+  final String temp;
+  final List<Map> hours;
+  final String date;
+
+  Weather({
+    required this.cityName,
+    required this.condition,
+    required this.temp,
+    required this.hours,
+    required this.date,
+  });
+
+  factory Weather.fromJson(dynamic json) {
+    return Weather(
+      cityName: json["info"]["tzinfo"]["name"] as String,
+      condition: json["fact"]["condition"] as String,
+      temp: json["fact"]["temp"] as String,
+      hours: json["forecasts"][0]["hours"] as List<Map>,
+      date: json["forecasts"][0]["date"] as String,
+    );
+  }
+
+  static List<Weather> weatherFromSnapshot(List snapshot) {
+    return snapshot.map(
+      (data) {
+        return Weather.fromJson(data);
+      },
+    ).toList();
+  }
+}
