34ba4a9de9b89d66936f18d9eac3413c6d93b965
faridun-dev <faridunf200630@gmail.com>
2024-09-12 15:01:17 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather_app_yandex_api
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/weather_app_yandex_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weather_app_yandex_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
